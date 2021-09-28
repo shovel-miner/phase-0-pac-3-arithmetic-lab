12a4c00ff75c3847a1a041217857ef7b22563d8a
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2021-09-28 13:29:00 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+number = 10
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+function increment(a){
+    return a + 1;
+}
+function decrement(a){
+    return a -1;
+}
+
+function makeInt(n){
+     parseInt(n,10);
+     return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    parseFloat(n,10)
+    return parseFloat(n,10);
+}
